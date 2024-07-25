997ac2a25c767d44aa663424309160bf5d4e7f9e
Sergii Semenets <serhey.semenets@gmail.com>
2024-07-25 13:21:50 +0300

    add urls

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from cinema.models import Movie
+from cinema.views import movie_list, movie_detail
 
 
 app_name = 'cinema'
 
 urlpatterns = [
-    path('movies/', movies_list, name='movies_list'),
+    path('movies/', movie_list, name='movie_list'),
+    path("movies/<int:pk>/", movie_detail, name="movie_detail"),
 ]
